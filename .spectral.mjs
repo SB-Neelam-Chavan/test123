bc058db3267e1556581984a288dafdc6c4e23030
neelam.chavan@smartbear.com <neelam.chavan@smartbear.com>
2025-02-06 19:02:18 +0530

    Added 142 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19856.stoplight-dev.com/api/v1/projects/cHJqOjQzMQ/spectral.js?branch_slug=main&token=6875b64a-ae92-48cd-bd09-3075fe9e2105";
+export default { extends: ruleset };
---
+++ docs/acceptor/getting-started.md
@@ -0,0 +1,8 @@
+---
+stoplight-id: iwpuqeq3w5xtx
+---
+
+# Introduction
+
+This API allows Acceptors (Agents and Merchants) to perform operations and transactions on Skom It Bank master platform.
+
